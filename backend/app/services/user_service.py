ed4c33fa046fede0a92371ce854c64b840dce407
Alejandro Roman <romanibanez.alex@gmail.com>
2024-12-26 17:51:46 +0100

    Tested and fix issues with avatar on local, now it works on both S3 and local again + some PEP8

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,11 +1,10 @@
 import os
-
+from typing import List, Dict
 import boto3
 import requests
 from botocore.exceptions import NoCredentialsError
 from flask import current_app
 from werkzeug.utils import secure_filename
-from typing import List, Dict
 from .. import db
 from ..models.user_model import User, BasketItem
 from ..models.product_model import Product
@@ -31,6 +30,9 @@ def is_ec2_instance():
 
 
 def get_s3_client():
+    """
+    Returns an S3 client based on the storage option.
+    """
     if USE_S3_STORAGE:
         if is_ec2_instance():
             session = boto3.Session()
@@ -58,13 +60,10 @@ def get_avatar_url(user):
             return user.avatar
         return DEFAULT_AVATAR_S3_URL
     else:
-        # Use local storage path
         if user.avatar:
             local_avatar_path = os.path.join(UPLOAD_FOLDER, user.avatar)
             if os.path.exists(local_avatar_path):
-                # Serve avatar from local server
                 return f"/api/me/avatar/{user.avatar}"
-        # Fallback to the default avatar
         return f"/api/me/avatar/{DEFAULT_AVATAR}"
 
 
@@ -88,6 +87,7 @@ def get_all_users() -> list:
 
     current_app.logger.info(f"Retrieved {len(users)} users.")
     return user_list
+
 
 def get_user_info(user_id: int) -> dict:
     """
@@ -115,6 +115,16 @@ def get_user_info(user_id: int) -> dict:
 
 
 def add_to_favorites(user_id: int, product_id: int) -> dict:
+    """
+    Adds a product to the user's list of favorite products in the database.
+
+    Args:
+        user_id (int): The ID of the user.
+        product_id (int): The ID of the product to add.
+
+    Returns:
+        dict: The raw result of the update operation.
+    """
     user = User.query.get(user_id)
     if not user:
         current_app.logger.error(f"User with ID {user_id} not found.")
